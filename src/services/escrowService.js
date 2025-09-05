1fd66027a09f52cc858ff04d3a4ddca8e0690f7c
Anthony Mania <amj2544@tutanota.com>
2025-09-05 19:12:21 +0200

    Ngrok Config

--- src/services/escrowService.js
+++ src/services/escrowService.js
@@ -5,7 +5,7 @@ import { db } from "../firebase.js"; // adapte le chemin selon ton projet
 
 // const API_URL = "http://localhost:4242";
 // const API_URL = "http://192.168.1.42:4242/api/stripe";
-const ESCROW_API_URL = "http://192.168.1.42:4242"; // sans /api/stripe
+const ESCROW_API_URL = "https://1cc1ec2c6230.ngrok-free.app"; // sans /api/stripe
 
 
 /**
