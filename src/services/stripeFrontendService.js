d1a6931b07dec15da9b6e3ac2b1270fab44fca19
Anthony Mania <amj2544@tutanota.com>
2025-09-05 18:32:26 +0200

    In progressmodal Bug LocalHost

--- src/services/stripeFrontendService.js
+++ src/services/stripeFrontendService.js
@@ -1,6 +1,7 @@
 // const API_URL = "http://localhost:4242/api/stripe";
 // const API_URL = "http://192.168.1.42:4242/api/stripe";
-const STRIPE_API_URL = "http://192.168.1.42:4242/api/stripe";
+// const STRIPE_API_URL = "http://192.168.1.42:4242/api/stripe";
+export const STRIPE_API_URL = "http://192.168.1.42:4242/api/stripe";
 
 
 export const createStripeAccountForSolidaire = async () => {
